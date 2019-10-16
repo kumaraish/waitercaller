4a0cd9857177b8116a7d30014ede91e5d3189e51
kumaraish <kumar.aish2010@gmail.com>
2019-10-16 10:59:04 +0000

    authentication

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # waitercaller
 
-On your apache server, copy the waitercaller.conf to /etc/httpd/conf.d
+Bootstrap  3.3.5 - CSS framework
+Python 2.7.5 / Flask 1.1.1 - Backend Web framework
+
+Deployment-
+On your Apache httpd server, 
+1. clone the repo in /var/www
+2. copy waitercaller.conf to /etc/httpd/conf.d
+3. restart apache
--- waitercaller.py
+++ waitercaller.py
@@ -1,10 +1,75 @@
 from flask import Flask
+from flask import render_template
+from flask import redirect
+from flask import url_for
+from flask import request
+from flask_login import LoginManager
+from flask_login import login_required
+from flask_login import login_user
+from flask_login import logout_user
+from mockdbhelper import MockDBHelper as DBHelper
+from user import User
+from passwordhelper import PasswordHelper
+
+DB = DBHelper()
+PH = PasswordHelper()
 
 app = Flask(__name__)
+login_manager = LoginManager(app)
+
+app.secret_key = 'ybbdJo1JRnFbWWVcnZGvlZ1BaVqLAqpOImjEzXXJZEjUb+0brjEpTzWvU+BrHOaFyk+5y7UMJEan'
+
 
 @app.route("/")
-def index():
-    return "Under construction"
+def home():
+    return render_template("home.html")
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    email = request.form.get("email")
+    pw1 = request.form.get("password")
+    pw2 = request.form.get("password2")
+    if not pw1 == pw2:
+        return redirect(url_for('home'))
+    if DB.get_user(email):
+        return redirect(url_for('home'))
+    salt = PH.get_salt()
+    hashed = PH.get_hash(pw1 + salt)
+    DB.add_user(email, salt, hashed)
+    return redirect(url_for('home'))
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.form.get("email")
+    password = request.form.get("password")
+    stored_user = DB.get_user(email)
+    if stored_user and PH.validate_password(password, stored_user['salt'], stored_user['hashed']):
+        user = User(email)
+        login_user(user, remember=True)
+        return redirect(url_for('account'))
+    return home()
+
+
+@app.route("/account")
+@login_required
+def account():
+    return "You are logged in"
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("home"))
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user_password = DB.get_user(user_id)
+    if user_password:
+        return User(user_id)
+
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', port=3000, debug=True)
+    app.run(host='0.0.0.0', port=3000, debug=True)
